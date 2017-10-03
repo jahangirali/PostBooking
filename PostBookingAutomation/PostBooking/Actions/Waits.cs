14df57f9d2eabb9a0ae3970977f230f4af8d80bd
jahangira <jahangir.ali@easyjet.com>
2017-10-03 17:42:51 +0100

    Added SearchPodPage

--- PostBookingAutomation/PostBooking/Actions/Waits.cs
+++ PostBookingAutomation/PostBooking/Actions/Waits.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium.Support;
 
 namespace PostBooking.Actions
 {
     public class Waits
     {
 
+
+        public void WaitForElement()
+        {
+            
+        }
     }
 }
---
+++ PostBookingAutomation/PostBooking/Pages/SearchPodPage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace PostBooking.Pages
+{
+    public class SearchPodPage: TestBase
+    {
+        private IWebDriver Driver { get; }
+        private static readonly By PageSelector = By.CssSelector("div[class='drawer-section sign-in']");
+
+        public SearchPodPage(IWebDriver driver) : base()
+        {
+            Driver = driver;
+            return;
+        }
+
+    }
+}
